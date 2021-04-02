16021657830f3f77e0a2510dee0c504d5adbc6fb
Matt Strautmann <matt.strautmann@gmail.com>
2021-04-02 15:41:50 -0700

    Update execute.js

--- .shipyard/execute.js
+++ .shipyard/execute.js
@@ -6,6 +6,7 @@ var logName = process.env.LOG_NAME
 var obj = JSON.parse(fs.readFileSync('./environments.json', 'utf8'));
 var TAGS = obj.tags;
 
+console.log(`json_file: ${obj}`);
 console.log(`TAGS: ${TAGS}`);
 
 cmd = `gpg --quiet --batch --yes --decrypt --passphrase=$CREDENTIALS_GPG_PASSPHRASE --output  .df-credentials.json df-credentials.gpg;
