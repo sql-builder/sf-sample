ccdb03ad36569b2e4b186e7019333069fdefcc25
Matt Strautmann <matt.strautmann@gmail.com>
2021-04-02 15:37:28 -0700

    Update execute.js

--- .shipyard/execute.js
+++ .shipyard/execute.js
@@ -2,38 +2,13 @@ var fs = require('fs');
 var exec = require("child_process").exec;
 var logName = process.env.LOG_NAME
 
-// Get tag list for run if available
-// const getJsonFile = (filePath, encoding = 'utf8') => (
-// //    new Promise((resolve, reject) => {
-//    (resolve, reject) => {
-//       fs.readFile(filePath, encoding, (err, contents) => {
-//           if(err) {
-//              return reject(err);
-//           }
-//           resolve(contents);
-//       });
-//    })
-//      .then(JSON.parse)
-// );
-// const data = getJsonFile('./environments.json');
 
-// function jsonParser(stringValue) {
-
-//  var string = JSON.stringify(stringValue);
-//  var objectValue = JSON.parse(string);
-//  return objectValue[stringValue];
-// };
-// const keyFilter = (key) => (item) => (item.key === key);
-// const tagList = data.find(keyFilter('tags'));
-// var obj = JSON.parse(data);
 var obj = JSON.parse(fs.readFileSync('./environments.json', 'utf8'));
-var tags = obj.tags;
-// var tagList = jsonParser('tags');
-
+var TAGS = obj.tags;
 
 cmd = `gpg --quiet --batch --yes --decrypt --passphrase=$CREDENTIALS_GPG_PASSPHRASE --output  .df-credentials.json df-credentials.gpg;
 dataform install;
-dataform run --json ${tags};`
+dataform run --json $TAGS;`
 
 exec(cmd, {cwd: process.env.PROJECT_LOCATION, env: {'PROJECT_LOCATION': process.env.PROJECT_LOCATION, 'CREDENTIALS_GPG_PASSPHRASE': process.env.CREDENTIALS_GPG_PASSPHRASE, 'PATH': process.env.PATH}}, (error, stdout, stderr) => {
     if (error) {
